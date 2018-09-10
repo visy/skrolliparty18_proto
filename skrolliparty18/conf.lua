486af523c9021c3a56570ba1db88af8917e77476
Visa-Valtteri Pimiä <vp79799@gmail.com>
2018-09-10 20:39:20 +0300

    skrollidemo

---
+++ skrolliparty18/conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.window.width = 960
+    t.window.height = 720
+    t.window.msaa = 0
+    t.window.highdpi = false
+    t.window.title = "Skrolliparty 2018 demo"
+end
---
+++ skrolliparty18/main.lua
@@ -0,0 +1,50 @@
+local moonshine = require 'moonshine'
+
+local t = 0
+ 
+function love.load()
+    canvas = love.graphics.newCanvas(320, 240)
+
+	effect = moonshine(moonshine.effects.godsray)
+
+	mainFont = love.graphics.newFont("main.ttf", 20)
+end
+
+function do_effect() 
+	love.graphics.push()
+	    love.graphics.clear()
+		love.graphics.setFont(mainFont)
+
+	    love.graphics.setColor(1, 1, 1, 1.0)
+	    love.graphics.rectangle('fill', 50, 30, 200, 100)
+
+	    love.graphics.setColor(1, 0, 0, 1.0)
+
+		love.graphics.rectangle("fill", 150+math.sin(t*2.)*30,50, 100,100)
+
+	    love.graphics.setColor(0, 1, 0, 1.0)
+
+	    love.graphics.print('Hello World!', 160+math.cos(t*3)*30, 150)
+	love.graphics.pop()
+end
+
+function love.draw()
+    love.graphics.setCanvas(canvas)
+    	do_effect()
+    love.graphics.setCanvas()
+
+    love.graphics.clear()
+
+    love.graphics.setColor(1, 1, 1, 1.0)
+	effect(function()
+		love.graphics.push()
+		love.graphics.scale(3.0, 3.0)
+	    love.graphics.draw(canvas)
+		love.graphics.pop()
+	end)
+
+end
+
+function love.update(dt)
+	t = t + dt
+end
